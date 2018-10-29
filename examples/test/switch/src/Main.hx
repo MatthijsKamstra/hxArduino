ae44095e5e419951c1c0338862d39f65ba3bcc1e
Matthijs Kamstra <matthijs@fonk.amsterdam>
2018-10-29 12:54:40 +0100

    not to make it more complex...

--- examples/test/switch/src/Main.hx
+++ examples/test/switch/src/Main.hx
@@ -4,20 +4,20 @@ class Main {
 
 
 	public function setup() {
-		var str = 'foo';
-		switch (str) {
+		var _str = 'foo';
+		switch (_str) {
 			case 'foo': Serial.println ('foo');
 			case 'bar': Serial.println ('bar');
-			default : Serial.println ("case '"+str+"': trace ('"+str+"');");
+			default : Serial.println ("case '"+_str+"': trace ('"+_str+"');");
 		}
 
-		var int = 1;
-		switch (int) {
+		var _int = 1;
+		switch (_int) {
 			case 0: Serial.println ('0');
 			case 1: Serial.println ('1');
 			case 2: Serial.println ('2');
 			case 3: Serial.println ('3');
-			default : Serial.println ("case '"+str+"': trace ('"+str+"');");
+			default : Serial.println ("case "+_int+": trace ("+_int+");");
 		}
 	}
 
